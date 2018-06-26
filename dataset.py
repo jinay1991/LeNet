b8212251dca95f90c9eff41460174a3dfdfbd961
Jinay Patel <jinay1991@gmail.com>
2018-06-26 20:47:38 +0530

    Added Data Augmentations

--- dataset.py
+++ dataset.py
@@ -1,8 +1,13 @@
-from urllib.request import urlretrieve
 import os
-import zipfile
 import pickle
+import zipfile
+from urllib.request import urlretrieve
+
 import numpy as np
+from scipy.ndimage import rotate
+from sklearn.utils import shuffle
+import cv2
+
 
 def download(url, file):
     """
@@ -23,6 +28,78 @@ def uncompress(file):
     zip_ref = zipfile.ZipFile(file, 'r')
     zip_ref.extractall('.')
     zip_ref.close()
+
+
+
+def generate_additional_data_rotate(x, y):
+    for i in range(43):
+        indexes = y == i
+        count = np.sum(indexes)
+        x_train_sample = x[indexes]
+        y_train_sample = y[indexes]
+        if count < 500:
+            # three random angles between -10 to 10 degress
+            three_random_angles = 20.0*np.random.random_sample(3) - 10.0
+            x1 = rotate(x_train_sample, three_random_angles[0], axes=(1,2), reshape=False)
+            x2 = rotate(x_train_sample, three_random_angles[1], axes=(1,2), reshape=False)
+            x3 = rotate(x_train_sample, three_random_angles[2], axes=(1,2), reshape=False)
+            X_additional = np.concatenate((x1, x2, x3))
+            x = np.concatenate((x, X_additional))
+            y = np.concatenate((y, y_train_sample, y_train_sample, y_train_sample))
+        elif count < 1000:
+            # one random angles between -10 to 10 degress
+            one_random_angle = 20.0*np.random.random_sample(1) - 10.0
+            x1 = rotate(x_train_sample, one_random_angle[0], axes=(1,2), reshape=False)
+            x = np.concatenate((x, x1))
+            y = np.concatenate((y, y_train_sample))
+
+    return x, y
+
+def generate_additional_data_translate(x, y):
+    num_total = y.shape[0]
+    first_break = int(num_total/3)
+    second_break = 2*first_break
+    x_sample_1 = x[0:first_break]
+    x_sample_2 = x[first_break:second_break]
+    x_sample_3 = x[second_break:]
+    y1 = y[0:first_break]
+    y2 = y[first_break:second_break]
+    y3 = y[second_break:]
+
+    random_integer = 0
+    while(random_integer == 0):
+        random_integer = np.random.randint(-4,4)
+    x1 = np.roll(x_sample_1, random_integer, axis=2)
+
+    random_integer = 0
+    while(random_integer == 0):
+        random_integer = np.random.randint(-4,4)
+    x2 = np.roll(x_sample_2, random_integer, axis=2)
+
+    random_integer = 0
+    while(random_integer == 0):
+        random_integer = np.random.randint(-4,4)
+    x3 = np.roll(x_sample_3, random_integer, axis=2)
+
+    x = np.concatenate((x, x1, x2, x3))
+    y = np.concatenate((y, y1, y2, y3))
+    return x, y
+
+def auto_contrast(img):
+    '''
+    Adjust contrast for the image automatically
+    '''
+    # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+    clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(8, 8))
+
+    lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)  # convert from BGR to LAB color space
+    l, a, b = cv2.split(lab)  # split on 3 different channels
+
+    l2 = clahe.apply(l)  # apply CLAHE to the L-channel
+
+    lab = cv2.merge((l2, a, b))  # merge channels
+    img2 = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)  # convert from LAB to RGB
+    return img2
 
 def load_data(url):
     download(url, 'traffic-sign-data.zip')
@@ -53,6 +130,14 @@ def load_data(url):
     print("Training Set:   {} samples".format(len(X_train)))
     print("Validation Set: {} samples".format(len(X_valid)))
     print("Test Set:       {} samples".format(len(X_test)))
+
+    # Perform Pre-processing for entire dataset (training, test, validation sets)
+    X_train, y_train = generate_additional_data_rotate(X_train, y_train)
+    X_train, y_train = generate_additional_data_translate(X_train, y_train)
+
+    # adjust contrast
+    X_train = np.array([auto_contrast(x) for x in X_train])
+    X_test = np.array([auto_contrast(x) for x in X_test])
 
     return X_train, y_train, X_test, y_test
 
